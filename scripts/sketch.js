d0ef1f80eeddcf54c63a90d4243fe0ccb9141c24
Abhinav <chandruabhi1968@gmail.com>
2022-02-13 15:00:39 +0530

    converting indentation from spaces to tabs

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -9,66 +9,66 @@ let notPressed = true;
 let boundary = [];
 
 function setup() {
-    createCanvas(w, h);
-    car1 = new Car(w / 2, h / 2);
+	createCanvas(w, h);
+	car1 = new Car(w / 2, h / 2);
 
-    boundary.push(new Boundary(0, 0, w, 0));
-    boundary.push(new Boundary(w, 0, w, h));
-    boundary.push(new Boundary(w, h, 0, h));
-    boundary.push(new Boundary(0, h, 0, 0));
+	boundary.push(new Boundary(0, 0, w, 0));
+	boundary.push(new Boundary(w, 0, w, h));
+	boundary.push(new Boundary(w, h, 0, h));
+	boundary.push(new Boundary(0, h, 0, 0));
 }
 
 function draw() {
-    background(51);
+	background(51);
 
-    if (keyIsPressed) {
-        if (key === "w") {
-            car1.gas();
-        }
-        if (key === "a") {
-            car1.rotateLeft();
-        }
-        if (key === "s") {
-            car1.brake();
-        }
-        if (key === "d") {
-            car1.rotateRight();
-        }
-        if (key === "c") {
-            notPressed = true;
-        }
-    }
-    else {
-        car1.angVelocity = 0;
-    }
-    car1.update();
-    car1.draw();
+	if (keyIsPressed) {
+		if (key === "w") {
+			car1.gas();
+		}
+		if (key === "a") {
+			car1.rotateLeft();
+		}
+		if (key === "s") {
+			car1.brake();
+		}
+		if (key === "d") {
+			car1.rotateRight();
+		}
+		if (key === "c") {
+			notPressed = true;
+		}
+	}
+	else {
+		car1.angVelocity = 0;
+	}
+	car1.update();
+	car1.draw();
 
-    for (let i = 0; i < boundary.length; i++) {
-        boundary[i].draw(255, 255, 255, 255);
-    }
+	for (let i = 0; i < boundary.length; i++) {
+		boundary[i].draw(255, 255, 255, 255);
+	}
 
-    for (let i = 0; i < boundary.length; i++)   {
-        for (let j = 0; j < 4; j++) {
-            car1.boundary[j].hits(boundary[i]);
-        }
-    }
+	for (let i = 0; i < boundary.length; i++)   {
+		for (let j = 0; j < 4; j++) {
+			car1.boundary[j].hits(boundary[i]);
+		}
+	}
 
 }
 
 function mousePressed() {
-    if (notPressed) {
-        temp1.x = mouseX;
-        temp1.y = mouseY;
-        notPressed = false;
-    }
-    else {
-        temp[0].x = temp1.x;
-        temp[0].y = temp1.y;
-        temp[1].x = mouseX;
-        temp[1].y = mouseY;
-        temp1.x = mouseX;
-        temp1.y = mouseY;
-        boundary.push(new Boundary(temp[0].x, temp[0].y, temp[1].x, temp[1].y))
-    }
+	if (notPressed) {
+		temp1.x = mouseX;
+		temp1.y = mouseY;
+		notPressed = false;
+	}
+	else {
+		temp[0].x = temp1.x;
+		temp[0].y = temp1.y;
+		temp[1].x = mouseX;
+		temp[1].y = mouseY;
+		temp1.x = mouseX;
+		temp1.y = mouseY;
+		boundary.push(new Boundary(temp[0].x, temp[0].y, temp[1].x, temp[1].y))
+	}
 }
